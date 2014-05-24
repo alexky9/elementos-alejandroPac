0c62b537ae68ffeda36b496c862793756c342a72
Alexkyn <Alexkyn@10.20.0.231>
2014-05-23 21:35:08 -0500

    Practica Elementos 1

---
+++ src/main/java/alexky/proyecto1/ContadorVisitas.java
@@ -0,0 +1,19 @@
+
+package alexky.proyecto1;
+
+public class ContadorVisitas {
+    
+    private static int contador;
+
+    public static int getContador() {
+        contador++;
+        return contador;
+    }
+
+    public void setContador(int contador) {
+        this.contador = contador;
+    }
+    
+}
+    
+    
